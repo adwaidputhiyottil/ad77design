4d6d0d35a16193a332017272663a7ba674d91b23
adwaidputhiyottil <adwaidpc0704@gmail.com>
2026-03-04 20:27:56 +0530

    fix: add vercel routing config

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,6 @@ function App() {
 
           {/* Admin Routes */}
           <Route path="/login" element={<Login />} />
-          <Route path="/test-page" element={<div className="p-20 text-center font-bold">Routing is actually working!</div>} />
           <Route 
             path="/admin/*" 
             element={
